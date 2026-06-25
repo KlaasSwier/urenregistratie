f9966ec0bb794b9c62afe49fc6214760f1e64053
KlaasSwier <info@klaasswierjr.nl>
2026-06-25 10:57:31 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
-// firebase-config.js — COMPAT
+// firebase-config.js — COMPAT variant
 const firebaseConfig = {
-  apiKey: "AIzaSyBE9_oNBQdjcNVqJgeEa_uOgm_zPvl2Z8o",
-  authDomain: "urenregistratie-9c74b.firebaseapp.com",
-  projectId: "urenregistratie-9c74b",
-  storageBucket: "urenregistratie-9c74b.appspot.com",
-  messagingSenderId: "868310860901",
-  appId: "1:868310860901:web:a72e3afd0ef7e2c48728c"
+  apiKey: "AIzaSyC7OtHEZBM60_HFipE4j5W6ghJuXyG3vIk",
+  authDomain: "urenregistratie-b4ffd.firebaseapp.com",
+  projectId: "urenregistratie-b4ffd",
+  storageBucket: "urenregistratie-b4ffd.firebasestorage.app",
+  messagingSenderId: "432575433985",
+  appId: "1:432575433985:web:b1f52c8381fdb0475e1f00"
 };
 
 firebase.initializeApp(firebaseConfig);
+
 window.auth = firebase.auth();
-window.db   = firebase.firestore();
+window.db = firebase.firestore();
